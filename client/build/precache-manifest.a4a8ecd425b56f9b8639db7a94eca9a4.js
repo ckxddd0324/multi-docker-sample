6b5dd9a642f922f4b90c930043a9343349f7a1eb
cheuk.chan <cheukkwan_chan@outlook.com>
2019-04-17 00:13:59 -0400

    update client folder/ missing files

---
+++ client/build/precache-manifest.a4a8ecd425b56f9b8639db7a94eca9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdf09dc7d86a3a646a3c",
+    "url": "/static/css/main.f346f035.chunk.css"
+  },
+  {
+    "revision": "fdf09dc7d86a3a646a3c",
+    "url": "/static/js/main.e5aee458.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48690f6720db52e630ed",
+    "url": "/static/js/2.7fc6c665.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cbd185ec9bd5edb53b4e4ecaf39f3319",
+    "url": "/index.html"
+  }
+];
